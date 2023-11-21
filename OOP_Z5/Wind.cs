c83989161beaacd9ff53ec48b0899a68da1864aa
Vampir <nik.aleks.ru@yandex.ru>
2023-11-21 17:45:56 +0300

    first commit

---
+++ OOP_Z5/Wind.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Z5
+{
+    class Wind
+    {
+        double _x;
+        double _y;
+
+        public Wind(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
